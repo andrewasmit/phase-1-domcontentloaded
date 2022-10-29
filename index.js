2a22c7b03483a582c492cdd160cee6dbb0781ef5
andrew <andrewasmit@hotmail.com>
2022-10-28 19:22:25 -0500

    commit tree 0f4ac9012ddc55d62eaffa166389da38b0b39999

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// // As one line
+document.addEventListener("DOMContentLoaded", function (){
+    let p= document.getElementById('text')
+    p.textContent='This is really cool!'
+});
+
+// // Defining the call back outside the event listener
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log('After!');
+//     findText();
+// });
+
+// function findText(){
+//     let p= document.getElementById('text')
+//     p.innerHTML='This is really cool!'
+// };
+
+// console.log("Before!")
